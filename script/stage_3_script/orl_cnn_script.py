ebc75164e751f518b6291826e17842039c109e5a
Liam Hepler Mackey <liamhm4@gmail.com>
2023-02-16 14:51:40 -0800

    fix: included torchvision in poetry

---
+++ script/stage_3_script/orl_cnn_script.py
@@ -0,0 +1,153 @@
+import os
+from code.base_class.dataset import datasetConfig
+from code.base_class.evaluate import EvaluateConfig
+from code.base_class.method import methodConfig
+from code.base_class.result import resultConfig
+from code.base_class.setting import SettingConfig
+from code.lib.comet_listeners import CometConfig, CometExperimentTracker
+from code.lib.notifier import (
+    DatasetNotifier,
+    EvaluateNotifier,
+    MethodNotifier,
+    MLEventType,
+    ResultNotifier,
+    SettingNotifier,
+)
+
+# from code.lib.util.device import get_device
+from code.stage_3_code.Dataset_Loader import ValidatedPickleLoader
+from code.stage_3_code.Evaluate_F1 import Evaluate_F1
+from code.stage_3_code.Method_CNN import MethodCNN
+from code.stage_3_code.Result_Saver import Result_Saver
+from code.stage_3_code.Setting_Train_Test_Split import Setting_Train_Test_Split
+
+import numpy as np
+import torch
+from torchmetrics import MetricCollection
+from torchmetrics.classification import (
+    MulticlassAccuracy,
+    MulticlassF1Score,
+    MulticlassPrecision,
+    MulticlassRecall,
+)
+
+# ---- Convolutional - Neural - Network script ----
+
+
+def main():
+    # ---- parameter section -------------------------------
+    np.random.seed(2)
+    torch.manual_seed(2)
+    # ------------------------------------------------------
+    # ---- objection initialization setction ---------------
+
+    # device: torch.device = get_device()
+    device: torch.device = torch.device("cpu")
+
+    algorithm_type = "CNN"
+    dataset_name = "CIFAR"
+
+    config = CometConfig(
+        {
+            "api_key": os.environ["COMET_API_KEY"],
+            "project_name": "some-dl-models",
+            "workspace": "ecs189g",
+        }
+    )
+
+    experiment_tracker = CometExperimentTracker(config, dry_run=False)
+
+    d_config = datasetConfig(
+        {
+            "name": dataset_name,
+            "description": "...data description...",
+            "source_folder_path": "data/stage_3_data/",
+            "source_file_name": dataset_name,
+            "device": device,
+        }
+    )
+
+    r_config = resultConfig(
+        {
+            "name": f"{dataset_name}-{algorithm_type}-result",
+            "description": "...data description...",
+            "destination_folder_path": f"result/stage_3_result/{algorithm_type}_",
+            "destination_file_name": "prediction_result",
+        }
+    )
+    m_config = methodConfig(
+        {
+            "name": f"{algorithm_type}-method",
+            "description": "This is a convolutional neural network",
+            "hyperparameters": {
+                "max_epoch": 500,
+                "learning_rate": 1e-3,
+                "conv_channels_in_dim": 3,
+                "conv_channels_out_dim_0": 6,
+                "conv_channels_out_dim_1": 16,
+                "conv_kernel_size": 5,
+                "pool_kernel_size": 2,
+                "pool_stride": 2,
+                "batch_size": 40,
+                "output_dim_0": 84,
+                "output_dim_1": 20,
+                "output_dim_2": 10,
+            },
+        }
+    )
+    s_config = SettingConfig(
+        {
+            "name": "Setting_Train_Test_Split",
+            "description": "This setting enables us to divide our data in sections",
+            "device": device,
+        }
+    )
+
+    e_config = EvaluateConfig(
+        {"name": "recall", "description": "This is my recall object evaluator"}
+    )
+
+    d_notifier = DatasetNotifier()
+    d_notifier.subscribe(experiment_tracker.dataset_listener, MLEventType("load"))
+    data_obj = ValidatedPickleLoader(d_config, d_notifier)
+
+    m_notifier = MethodNotifier()
+    m_notifier.subscribe(experiment_tracker.method_listener, MLEventType("method"))
+    batch_metrics = MetricCollection(
+        [
+            MulticlassAccuracy(num_classes=m_config["hyperparameters"]["output_dim_2"]).to(device),
+            MulticlassF1Score(num_classes=m_config["hyperparameters"]["output_dim_2"]).to(device),
+            MulticlassPrecision(num_classes=m_config["hyperparameters"]["output_dim_2"]).to(device),
+            MulticlassRecall(num_classes=m_config["hyperparameters"]["output_dim_2"]).to(device),
+        ]
+    )
+
+    method_obj = MethodCNN(m_config, m_notifier, batch_metrics)
+
+    r_notifier = ResultNotifier()
+    r_notifier.subscribe(experiment_tracker.result_listener, MLEventType("save"))
+    result_obj = Result_Saver(r_config, r_notifier)
+
+    s_notifier = SettingNotifier()
+    s_notifier.subscribe(experiment_tracker.setting_listener, MLEventType("setting"))
+    setting_obj = Setting_Train_Test_Split(s_config, s_notifier)
+
+    e_notifier = EvaluateNotifier()
+    e_notifier.subscribe(experiment_tracker.evaluation_listener, MLEventType("evaluate"))
+    final_evaluation = Evaluate_F1(e_config, e_notifier)
+
+    # ------------------------------------------------------
+
+    # ---- running section ---------------------------------
+    print("************ Start ************")
+    setting_obj.prepare(data_obj, method_obj, result_obj, final_evaluation)
+    setting_obj.print_setup_summary()
+    mean_score, std_score = setting_obj.load_run_save_evaluate()
+    print("************ Overall Performance ************")
+    print(f"{algorithm_type} Accuracy: " + str(mean_score) + " +/- " + str(std_score))
+    print("************ Finish ************")
+    # ------------------------------------------------------
+
+
+if __name__ == "__main__":
+    main()
