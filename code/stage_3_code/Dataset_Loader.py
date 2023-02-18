ff63cc4ee6f41d881114b02a54c6f7b89bbf5028
Matthew Schulz <74387737+mkschulz9@users.noreply.github.com>
2023-02-18 10:01:38 -0800

    feat(MNIST_CNN): almost working model

--- code/stage_3_code/Dataset_Loader.py
+++ code/stage_3_code/Dataset_Loader.py
@@ -18,12 +18,13 @@ from torch.utils.data import Dataset
 
 
 class LoadedDataset(Dataset):
-    def __init__(self, X, y, transform=None, train=True):
+    def __init__(self, X, y, transform=None, train=True, toByte=False):
 
         self.X = X
         self.y = y
         self.transform = transform
         self.train = train
+        self.toByte = toByte
 
     def __len__(self):
         return len(self.X)
@@ -35,8 +36,8 @@ class LoadedDataset(Dataset):
 
         sample = {"image": image, "label": label}
 
-        if self.transform:
-            sample["image"] = self.transform(sample["image"])
+        # convert tensor to bytes (if needed)
+        sample["image"] = self.transform(sample["image"].byte()) if self.transform and self.toByte else self.transform(sample["image"])
 
         return sample
 
@@ -81,7 +82,7 @@ class ValidatedPickleLoader(dataset):
         self.samples_to_verify = _samples_to_verify
         self.device = config["device"]
 
-    def load(self) -> dict:
+    def load(self, deactivateReorder = False) -> dict:
 
         data = self.real_loader.load()
 
@@ -104,6 +105,23 @@ class ValidatedPickleLoader(dataset):
         }
 
         v.validate(batch)
+
+        # disables reordering of axes in dataset
+        if deactivateReorder: 
+            return {
+                "X_train": torch.LongTensor(
+                    np.array([example["image"] for example in data["train"]])
+                ).to(self.device),
+                "y_train": torch.LongTensor(
+                    np.array([example["label"] for example in data["train"]])
+                ).to(self.device),
+                "X_test": torch.LongTensor(
+                    np.array([example["image"] for example in data["test"]])
+                ).to(self.device),
+                "y_test": torch.LongTensor(
+                    np.array([example["label"] for example in data["test"]])
+                ).to(self.device),
+            }
 
         return {
             "X_train": torch.FloatTensor(
